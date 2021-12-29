4c49546e04527f36485c001652efafa46d3fb667
TheNeverMan <thenevermanever@qmail.com>
2021-12-29 13:15:41 +0100

    hide password

--- src/server/config.php
+++ src/server/config.php
@@ -1,6 +1,6 @@
 <?php
 $servername = "localhost";
-$username = "id18164401_tttouser";
-$password = "l^m&lPYQ-=Oj5ig%";
+$username = "username";
+$password = "password";
 $minimum_supported_version = 1;
 ?>
